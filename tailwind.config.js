281982f45e9e389023152228844714e4318d3a85
nopal <naufaladyat234@gmail.com>
2023-01-15 21:59:52 +0700

    first Lenovo

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'garamond': ['garamond']},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'my-black': '#01071d',
+      'my-crown': '#fef2cc',
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
